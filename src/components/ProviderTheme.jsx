b593bb3137bebf0faa26ad10fd4fca0aac3c300a
Nico Schönfeld <75105918+NicoSchonfeld@users.noreply.github.com>
2023-08-28 17:00:38 -0300

    Save Project

--- src/components/ProviderTheme.jsx
+++ src/components/ProviderTheme.jsx
@@ -3,7 +3,14 @@
 import { ThemeProvider } from "next-themes";
 
 const ProviderTheme = ({ children }) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <>
+      {/* <ThemeProvider>{children}</ThemeProvider> */}
+      <div className="w-full h-screen flex items-center justify-center text-6xl font-bold">
+        Maintenance
+      </div>
+    </>
+  );
 };
 
 export default ProviderTheme;

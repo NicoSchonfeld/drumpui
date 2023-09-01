ea5f4356373d6bc96ba0005a596b312981d9e96b
Nico Schönfeld <75105918+NicoSchonfeld@users.noreply.github.com>
2023-09-01 14:39:57 -0300

    Save Project

--- src/components/CodeComponentDemoHomeCard.jsx
+++ src/components/CodeComponentDemoHomeCard.jsx
@@ -3,14 +3,14 @@
 import React from "react";
 
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { atomOneDarkReasonable } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const CodeComponentDemoHomeCard = ({ children }) => {
   return (
     <>
       <SyntaxHighlighter
         language="jsx"
-        style={atomOneDarkReasonable}
+        style={atomOneDark}
         customStyle={{ background: "transparent" }}
         className={`scrollbar-thin scrollbar-thumb-white scrollbar-track-transparent`}
       >
